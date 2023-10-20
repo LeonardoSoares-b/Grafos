2c6a135f412c2bddf4aa0a712ef26916fd1a0e4d
LeonardoSoares-b <82275509+LeonardoSoares-b@users.noreply.github.com>
2023-10-20 14:23:19 -0300

    nova funcao

--- Entrada, saida,  variaveis e operadores/ex5.py
+++ Entrada, saida,  variaveis e operadores/ex5.py
@@ -1,6 +1,6 @@
 areaPintada=float(input("Tamanho da area a ser pintada em metros quadrados: "))
 litrosNecessarios=(1/3)*areaPintada
-latasNecessarias=int(litrosNecessarios/18)+1
+latasNecessarias=(litrosNecessarios//18)+1
 print(f"A area a ser pintada tem: {areaPintada} metros quadrados.")
 print(f"Serão necessarios: {litrosNecessarios} litros de tinta")
 print(f"serão necessarias: {latasNecessarias} latas de tinta")
--- aula2010/hashMap.py
+++ aula2010/hashMap.py
@@ -1,4 +1,9 @@
 user = {"nome": "George", "dnasc": "28/07/90"}
 print(user["nome"])
 user["email"]="george@ufop.edu.br"
-print(user["email"])
+t={"pt":{"hello word":"ola mundo","cor":"casa"},
+   "en":{"hello word":"hello word","cor":"casa"}}
+frutas=["A", "B", "C"]
+print("D" in frutas)
+print(5**2)
+print(5//2)
